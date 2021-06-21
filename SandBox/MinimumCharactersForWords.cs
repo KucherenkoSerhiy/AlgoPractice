36aeaccd406956e904b0c2c164229575efe718a4
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2021-06-21 15:52:04 +0200

    MinimumCharactersForWords

---
+++ SandBox/MinimumCharactersForWords.cs
@@ -0,0 +1,58 @@
+﻿namespace AlgoExpert
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class MinimumCharactersForWords
+    {
+        // O(n*w) Time | O(n*w) Space, where n = number of words and w = avg word length
+        public string[] Solve(string[] words)
+        {
+            var letterFrequencies = new Dictionary<char, int>();
+            foreach (var word in words)
+            {
+                var currentWordLetterFrequencies = this.RetrieveCurrentWordLetterFrequencies(word);
+
+                this.CountFrequencies(currentWordLetterFrequencies, letterFrequencies);
+            }
+            return this.ConvertToArray(letterFrequencies);
+        }
+
+        private Dictionary<char, int> RetrieveCurrentWordLetterFrequencies(string word)
+        {
+            var currentWordLetterFrequencies = new Dictionary<char, int>();
+            foreach (var symbol in word)
+            {
+                if (!currentWordLetterFrequencies.ContainsKey(symbol))
+                    currentWordLetterFrequencies.Add(symbol, 1);
+                else
+                    currentWordLetterFrequencies[symbol]++;
+            }
+
+            return currentWordLetterFrequencies;
+        }
+
+        private void CountFrequencies(Dictionary<char, int> currentWordLetterFrequencies, Dictionary<char, int> letterFrequencies)
+        {
+            foreach (var frequency in currentWordLetterFrequencies)
+            {
+                if (!letterFrequencies.ContainsKey(frequency.Key))
+                    letterFrequencies.Add(frequency.Key, frequency.Value);
+                else
+                    letterFrequencies[frequency.Key] = Math.Max(frequency.Value, letterFrequencies[frequency.Key]);
+            }
+        }
+
+        private string[] ConvertToArray(Dictionary<char, int> letterFrequencies)
+        {
+            var letters = new List<string>();
+            foreach (var (letter, frequency) in letterFrequencies)
+            {
+                for (int i = 1; i <= frequency; i++)
+                    letters.Add(letter.ToString());
+            }
+
+            return letters.ToArray();
+        }
+    }
+}
---
+++ SandBoxTest/MinimumCharactersForWordsTest.cs
@@ -0,0 +1,21 @@
+﻿namespace AlgoExpertTest
+{
+    using AlgoExpert;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class MinimumCharactersForWordsTest
+    {
+        [Test]
+        public void TestCase1()
+        {
+            var words = new[] {"this", "that", "did", "deed", "them!", "a"};
+            var expected = new[] {"!", "a", "d", "d", "e", "e", "h", "i", "m", "s", "t", "t"};
+            var sut = new MinimumCharactersForWords();
+            var actual = sut.Solve(words);
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

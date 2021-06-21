3f559269d45dc5a4c123b655c2e2588059931056
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2021-06-21 15:09:02 +0200

    SuffixTrie

--- SandBox/SuffixTrie.cs
+++ SandBox/SuffixTrie.cs
@@ -14,13 +14,41 @@
             PopulateSuffixTrieFrom(str);
         }
 
-        public void PopulateSuffixTrieFrom(string str) {
-            // TODO
+        // O(n^2) Time | O(n^2) Space, where n = length of the str
+        public void PopulateSuffixTrieFrom(string str)
+        {
+            for (var i = 0; i < str.Length; i++)
+            {
+                var letter = str[i];
+                if (!this.root.Children.ContainsKey(letter))
+                    this.root.Children[letter] = new TrieNode();
+
+                var currentNode = this.root.Children[letter];
+                for (int j = i + 1; j < str.Length; j++)
+                {
+                    var nextLetter = str[j];
+
+                    if (!currentNode.Children.ContainsKey(nextLetter))
+                        currentNode.Children[nextLetter] = new TrieNode();
+
+
+                    currentNode = currentNode.Children[nextLetter];
+                }
+                currentNode.Children[endSymbol] = null;
+            }
         }
 
-        public bool Contains(string str) {
-            // TODO
-            return false;
+        // O(n) Time | O(1) Space, where n = length of the str
+        public bool Contains(string str)
+        {
+            var currentNode = this.root;
+            foreach (var letter in str)
+            {
+                if (!currentNode.Children.ContainsKey(letter)) return false;
+                currentNode = currentNode.Children[letter];
+            }
+
+            return currentNode != null && currentNode.Children.ContainsKey(endSymbol);
         }
     }
 }
--- SandBoxTest/SuffixTrieTest.cs
+++ SandBoxTest/SuffixTrieTest.cs
@@ -1,5 +1,6 @@
 ﻿namespace AlgoExpertTest
 {
+    using AlgoExpert;
     using NUnit.Framework;
 
     [TestFixture]
@@ -9,7 +10,42 @@
         [Test]
         public void TestCase1()
         {
+            var str = "babc";
+            var sut = new SuffixTrie(str);
+            Assert.IsTrue(sut.Contains(str));
+        }
 
+        [Test]
+        public void TestCase2()
+        {
+            var str = "1234556789";
+            var sut = new SuffixTrie(str);
+            Assert.IsTrue(sut.Contains("9"));
+            Assert.IsTrue(sut.Contains("89"));
+            Assert.IsTrue(sut.Contains("789"));
+            Assert.IsTrue(sut.Contains("6789"));
+            Assert.IsTrue(sut.Contains("56789"));
+            Assert.IsFalse(sut.Contains("456789"));
+            Assert.IsFalse(sut.Contains("3456789"));
+            Assert.IsFalse(sut.Contains("23456789"));
+            Assert.IsFalse(sut.Contains("123456789"));
+            Assert.IsFalse(sut.Contains("45567"));
+        }
+
+        [Test]
+        public void TestCase3()
+        {
+            var str = "testtest";
+            var sut = new SuffixTrie(str);
+            Assert.IsTrue(sut.Contains("t"));
+            Assert.IsTrue(sut.Contains("st"));
+            Assert.IsTrue(sut.Contains("est"));
+            Assert.IsTrue(sut.Contains("test"));
+            Assert.IsTrue(sut.Contains("ttest"));
+            Assert.IsTrue(sut.Contains("sttest"));
+            Assert.IsTrue(sut.Contains("esttest"));
+            Assert.IsTrue(sut.Contains("testtest"));
+            Assert.IsFalse(sut.Contains("tt"));
         }
     }
 }

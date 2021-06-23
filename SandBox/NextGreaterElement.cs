7e8e1a60fb16e0050594a079fbdbc743770c6129
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2021-06-23 15:10:51 +0200

    NextGreaterElement

---
+++ SandBox/NextGreaterElement.cs
@@ -0,0 +1,40 @@
+﻿namespace AlgoExpert
+{
+    using System.Collections.Generic;
+
+    public class NextGreaterElement
+    {
+        // O(n) Time | O(n) Space, where n = length of the array
+        public int[] Solve(int[] array)
+        {
+            var solution = new int[array.Length];
+            for (int i = 0; i < solution.Length; i++)
+                solution[i] = -1;
+
+            if (array.Length <= 1)
+                return solution;
+            
+            var currentStack = new Stack<(int index, int value)>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                while (currentStack.Count > 0 && currentStack.Peek().value < array[i])
+                {
+                    var index = currentStack.Pop().index;
+                    solution[index] = array[i];
+                }
+
+                currentStack.Push((i, array[i]));
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                while (currentStack.Count > 0 && currentStack.Peek().value < array[i])
+                {
+                    var index = currentStack.Pop().index;
+                    solution[index] = array[i];
+                }
+            }
+
+            return solution;
+        }
+    }
+}
---
+++ SandBoxTest/NextGreaterElementTest.cs
@@ -0,0 +1,41 @@
+﻿namespace AlgoExpertTest
+{
+    using AlgoExpert;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class NextGreaterElementTest
+    {
+        [Test]
+        public void TestCase1()
+        {
+            var array = new[] { 2, 5, -3, -4, 6, 7, 2 };
+            var expected = new[] { 5, 6, 6, 6, 7, -1, 5 };
+            var sut = new NextGreaterElement();
+            var actual = sut.Solve(array);
+            actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+
+        [Test]
+        public void TestCase2()
+        {
+            var array = new[] { 10 };
+            var expected = new[] { -1 };
+            var sut = new NextGreaterElement();
+            var actual = sut.Solve(array);
+            actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+
+        [Test]
+        public void TestCase3()
+        {
+            var array = new[] { 1, 1, 1, 1 };
+            var expected = new[] { -1, -1, -1, -1 };
+            var sut = new NextGreaterElement();
+            var actual = sut.Solve(array);
+            actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+    }
+}

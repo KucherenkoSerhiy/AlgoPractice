ea5b888a0c493047bbbdc234a7717aedc5b394fb
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2021-06-25 19:50:26 +0200

    SubarraySort

---
+++ SandBox/hard/SubarraySort.cs
@@ -0,0 +1,46 @@
+﻿namespace AlgoExpert.hard
+{
+    using System;
+
+    public class SubarraySort
+    {
+        // O(n) Time | O(1) Space, where n = length of the array
+        public static int[] Solve(int[] array)
+        {
+            // Step 1: find the breakage (if not found, the array is sorted)
+            int left = 0;
+            while (left < array.Length - 1 && array[left] <= array[left + 1])
+                left++;
+            if (left == array.Length - 1) return new[] {-1, -1}; // Array is sorted
+
+            int right = array.Length -1;
+            while (right > 0 && array[right - 1] <= array[right])
+                right--;
+
+            // Step 2: determine at which indexes the found values belong
+            int currentLeft = array[left];
+            while (left > 0 && array[left - 1] >= currentLeft)
+                left --;
+            int currentRight = array[right];
+            while (right < array.Length - 1 && array[right + 1] <= currentRight)
+                right ++;
+
+            // Step 3: find the min and max values between left and right
+            int unsortedMin = int.MaxValue;
+            int unsortedMax = int.MinValue;
+            for (int i = left; i <= right; i++)
+            {
+                unsortedMin = Math.Min(array[i], unsortedMin);
+                unsortedMax = Math.Max(array[i], unsortedMax);
+            }
+
+            // Step 4: expand left and right up to where min and max values should be inserted
+            while (left > 0 && array[left - 1] > unsortedMin)
+                left --;
+            while (right < array.Length - 1 && array[right + 1] < unsortedMax)
+                right ++;
+
+            return new[] {left, right};
+        }
+    }
+}
---
+++ SandBoxTest/hard/SubarraySortTest.cs
@@ -0,0 +1,47 @@
+﻿namespace AlgoExpertTest.hard
+{
+    using AlgoExpert.hard;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class SubarraySortTest
+    {
+        [Test]
+        public void TestCase1()
+        {
+            var array = new[] {1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19};
+            var expected = new[] {3, 9};
+            var actual = SubarraySort.Solve(array);
+            actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+
+        [Test]
+        public void TestCase2()
+        {
+            var array = new[] {1, 2, 4, 7, 10, 11, 16, 18, 19};
+            var expected = new[] {-1, -1};
+            var actual = SubarraySort.Solve(array);
+            actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+
+        [Test]
+        public void TestCase3()
+        {
+            var array = new[] {1, 2, 4, 4, 3, -1, 99, 4, 4, 3, 7};
+            var expected = new[] {0, 10};
+            var actual = SubarraySort.Solve(array);
+            actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+
+        [Test]
+        public void TestCase4()
+        {
+            var array = new[] {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89};
+            var expected = new[] {-1, -1};
+            var actual = SubarraySort.Solve(array);
+            actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+    }
+}

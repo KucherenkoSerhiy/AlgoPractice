3332753c5b13fd50e3d519b4049fe500c1c667bf
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2021-06-25 20:48:46 +0200

    LargestRange

---
+++ SandBox/hard/LargestRange.cs
@@ -0,0 +1,41 @@
+﻿namespace AlgoExpert.hard
+{
+    using System.Collections.Generic;
+
+    public class LargestRange
+    {
+        // O(n) Time | O(n) Space, where n = length of the array
+        public static int[] Solve(int[] array) {
+            var hashTable = new Dictionary<int, bool>();
+            foreach (var value in array)
+                hashTable[value] = false;
+
+            int[] maxRange = null;
+            foreach (int value in array){
+                if (!hashTable[value]) { // skip the already count value
+                    hashTable[value] = true;
+
+                    // get the range
+                    int left;
+                    for (left = value - 1; hashTable.ContainsKey(left); left--)
+                        hashTable[left] = true;
+                    left++;
+
+                    int right;
+                    for (right = value + 1; hashTable.ContainsKey(right); right++)
+                        hashTable[right] = true;
+                    right--;
+        
+                    // compare and store obtained range
+                    if (maxRange == null || maxRange[1] - maxRange[0] < right - left)
+                    {
+                        maxRange ??= new[] {0, 0};
+                        maxRange[0] = left;
+                        maxRange[1] = right;
+                    }
+                }
+            }
+            return maxRange;
+        }
+    }
+}
---
+++ SandBoxTest/hard/LargestRangeTest.cs
@@ -0,0 +1,29 @@
+﻿namespace AlgoExpertTest.hard
+{
+    using AlgoExpert.hard;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class LargestRangeTest
+    {
+        [Test]
+        public void TestCase1()
+        {
+            var array = new[] {1, 11, 3, 0, 15, 5, 2, 4, 10, 7, 12, 6};
+            var expected = new[] {0, 7};
+            var actual = LargestRange.Solve(array);
+            actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+
+        [Test]
+        public void TestCase2()
+        {
+            var array = new[] {1, 1};
+            var expected = new[] {1, 1};
+            var actual = LargestRange.Solve(array);
+            actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+    }
+}

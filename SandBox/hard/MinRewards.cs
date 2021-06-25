828b52e743fb3eb6e2be2a9d88d57fc9a62fea76
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2021-06-25 22:19:00 +0200

    MinRewards

---
+++ SandBox/hard/MinRewards.cs
@@ -0,0 +1,29 @@
+﻿namespace AlgoExpert.hard
+{
+    using System;
+    using System.Linq;
+
+    public class MinRewards
+    {
+        // O(n) Time | O(1) Space, where n = length of the array
+        public static int Solve(int[] scores) {
+            // left swipe
+            for (int i = 0; i < scores.Length; i++)
+                Minimize(scores, i);
+
+            // right swipe
+            for (int i = scores.Length - 1; i >= 0; i--)
+                Minimize(scores, i);
+
+            return scores.Sum();
+        }
+
+        private static void Minimize(int[] scores, int i)
+        {
+            int leftNeighbor = i == 0 || scores[i - 1] > scores[i] ? 0 : scores[i - 1];
+            int rightNeighbor = i == scores.Length - 1 || scores[i + 1] > scores[i] ? 0 : scores[i + 1];
+            int minPossible = Math.Max(leftNeighbor, rightNeighbor) + 1;
+            scores[i] = minPossible;
+        }
+    }
+}
---
+++ SandBoxTest/hard/MinRewardsTest.cs
@@ -0,0 +1,19 @@
+﻿namespace AlgoExpertTest.hard
+{
+    using AlgoExpert.hard;
+    using NUnit.Framework;
+
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class MinRewardsTest
+    {
+        [Test]
+        public void TestCase1()
+        {
+            int[] scores = {8, 4, 2, 1, 3, 6, 7, 9, 5};
+            var expected = 25;
+            var actual = MinRewards.Solve(scores);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

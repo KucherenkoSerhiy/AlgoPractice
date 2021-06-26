dca47ea164cc359141eccb8300b071fa5e5655d3
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2021-06-26 14:34:25 +0200

    ZigzagTraverse

---
+++ SandBox/hard/ZigzagTraverse.cs
@@ -0,0 +1,63 @@
+﻿namespace AlgoExpert.hard
+{
+    using System.Collections.Generic;
+
+    public class ZigzagTraverse
+    {
+        // O(n*m) Time | O(n*m) Space, where n and m are width and length of the array respectively
+        public static List<int> Solve(List<List<int> > array)
+        {
+            if (array.Count == 0) return new List<int>();
+
+            int i = 0;
+            int j = 0;
+            bool descending = true;
+
+            var solution = new List<int>();
+            int numberOfElements = array.Count * array[0].Count;
+            
+            solution.Add(array[i][j]);
+            while (solution.Count < numberOfElements)
+            {
+                if (descending)
+                {
+                    // bottom-left
+                    while (i < array.Count - 1 && j > 0)
+                    {
+                        i++;
+                        j--;
+                        solution.Add(array[i][j]);
+                    }
+
+                    // bottom or right
+                    if (i < array.Count - 1)
+                        i++;
+                    else
+                        j++;
+                    solution.Add(array[i][j]);
+                    descending = false;
+                }
+                else
+                {
+                    // top-right
+                    while (i > 0 && j < array[0].Count - 1)
+                    {
+                        i--;
+                        j++;
+                        solution.Add(array[i][j]);
+                    }
+
+                    // right or bottom
+                    if (j < array[0].Count - 1)
+                        j++;
+                    else
+                        i++;
+                    solution.Add(array[i][j]);
+                    descending = true;
+                }
+            }
+
+            return solution;
+        }
+    }
+}
---
+++ SandBoxTest/hard/ZigzagTraverseTest.cs
@@ -0,0 +1,38 @@
+﻿namespace AlgoExpertTest.hard
+{
+    using System.Collections.Generic;
+    using AlgoExpert.hard;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class ZigzagTraverseTest
+    {
+        [Test]
+        public void TestCase1()
+        {
+            var array = new List<List<int>>
+            {
+                new List<int>{1, 3, 4, 10},
+                new List<int>{2, 5, 9, 11},
+                new List<int>{6, 8, 12, 15},
+                new List<int>{7, 13, 14, 16}
+            };
+            var expected = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+            var actual = ZigzagTraverse.Solve(array);
+            actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+        [Test]
+        public void TestCase2()
+        {
+            var array = new List<List<int>>
+            {
+                new List<int>{1, 2, 3, 4, 5}
+            };
+            var expected = new List<int> {1, 2, 3, 4, 5};
+            var actual = ZigzagTraverse.Solve(array);
+            actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+    }
+}

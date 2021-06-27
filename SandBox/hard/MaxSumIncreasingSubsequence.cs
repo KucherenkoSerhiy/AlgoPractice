67fa5c7efa830e0d549a7d13e6f5f0ede46b0cbb
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2021-06-27 19:20:47 +0200

    MaxSumIncreasingSubsequence

---
+++ SandBox/hard/MaxSumIncreasingSubsequence.cs
@@ -0,0 +1,47 @@
+﻿namespace AlgoExpert.hard
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class MaxSumIncreasingSubsequence
+    {
+        // O(n^2) Time | O(n) Space, where n = length of the array
+        public static List<List<int> > Solution(int[] array)
+        {
+            (int sum, int max, List<int> sequence) maxSum = (int.MinValue, int.MinValue, new List<int>());
+            var sums = new List<(int sum, int max, List<int> sequence)>();
+            foreach (var value in array)
+            {
+                (int sum, int max, List<int> sequence) newSum = (value, value, new List<int>());
+                (int sum, int max, List<int> sequence) currentAcceptableMaxSum = (int.MinValue, int.MinValue, new List<int>());
+                foreach (var existingSum in sums)
+                {
+                    if (value > existingSum.max && existingSum.sum > currentAcceptableMaxSum.sum)
+                    {
+                        currentAcceptableMaxSum = existingSum;
+                    }
+                }
+
+                if (currentAcceptableMaxSum.sum > 0)
+                {
+                    newSum.sum += currentAcceptableMaxSum.sum;
+                    newSum.max = Math.Max(currentAcceptableMaxSum.max, value);
+                    newSum.sequence.AddRange(currentAcceptableMaxSum.sequence);
+                }
+                
+                newSum.sequence.Add(value);
+
+                sums.Add(newSum);
+
+                if (maxSum.sum < newSum.sum)
+                    maxSum = newSum;
+            }
+
+            return new List<List<int>>
+            {
+                new List<int>{maxSum.sum},
+                maxSum.sequence
+            };
+        }
+    }
+}
---
+++ SandBoxTest/hard/MaxSumIncreasingSubsequenceTest.cs
@@ -0,0 +1,51 @@
+﻿namespace AlgoExpertTest.hard
+{
+    using System.Collections.Generic;
+    using AlgoExpert.hard;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class MaxSumIncreasingSubsequenceTest
+    {
+        [Test]
+        public void TestCase1()
+        {
+            var array = new[] {10, 70, 20, 30, 50, 11, 30};
+            var expected = new List<List<int>>
+            {
+                new List<int>{110}, // sum
+                new List<int>{10, 20, 30, 50} // sequence
+            };
+            var actual = MaxSumIncreasingSubsequence.Solution(array);
+            actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+
+        [Test]
+        public void TestCase2()
+        {
+            var array = new[] {-1};
+            var expected = new List<List<int>>
+            {
+                new List<int>{-1}, // sum
+                new List<int>{-1} // sequence
+            };
+            var actual = MaxSumIncreasingSubsequence.Solution(array);
+            actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+
+        [Test]
+        public void TestCase3()
+        {
+            var array = new[] {-5, -4, -3, -2, -1};
+            var expected = new List<List<int>>
+            {
+                new List<int>{-1}, // sum
+                new List<int>{-1} // sequence
+            };
+            var actual = MaxSumIncreasingSubsequence.Solution(array);
+            actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+    }
+}

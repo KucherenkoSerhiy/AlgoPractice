2af5affe8f9699e8d66a7aaba34152009cf2ab9d
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2021-06-29 14:34:05 +0200

    WaterArea

---
+++ SandBox/hard/WaterArea.cs
@@ -0,0 +1,35 @@
+﻿namespace AlgoExpert.hard
+{
+    using System;
+    using System.Linq;
+
+    public static class WaterArea
+    {
+        // O(n) Time | O(n) Space, where n is the length of the array
+        public static int Solve(int[] heights)
+        {
+            int[] maxPillarHeights = new int[heights.Length];
+
+            // left swipe
+            int currentMaxPillarHeight = 0;
+            for (int i = 0; i < heights.Length; i++)
+            {
+                if (currentMaxPillarHeight < heights[i])
+                    currentMaxPillarHeight = heights[i];
+                maxPillarHeights[i] = currentMaxPillarHeight;
+            }
+
+            // right swipe
+            currentMaxPillarHeight = 0;
+            for (int i = heights.Length - 1; i >= 0; i--)
+            {
+                if (currentMaxPillarHeight < heights[i])
+                    currentMaxPillarHeight = heights[i];
+                maxPillarHeights[i] = Math.Min(currentMaxPillarHeight, maxPillarHeights[i]);
+            }
+
+            int sum = heights.Select((t, i) => maxPillarHeights[i] - t).Sum();
+            return sum;
+        }
+    }
+}
---
+++ SandBoxTest/hard/WaterAreaTest.cs
@@ -0,0 +1,19 @@
+﻿namespace AlgoExpertTest.hard
+{
+    using AlgoExpert.hard;
+    using NUnit.Framework;
+
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class WaterAreaTest
+    {
+        [Test]
+        public void TestCase1()
+        {
+            int[] heights = {0, 8, 0, 0, 5, 0, 0, 10, 0, 0, 1, 1, 0, 3};
+            var expected = 48;
+            var actual = WaterArea.Solve(heights);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

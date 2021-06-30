fe22d489ee34172befb149f995d19e6f6fc280e8
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2021-06-30 02:43:52 +0200

    KnapsackProblem

---
+++ SandBox/hard/KnapsackProblem.cs
@@ -0,0 +1,112 @@
+﻿namespace AlgoExpert.hard
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class KnapsackProblem
+    {
+        internal struct TableRegistry
+        {
+            internal int LastAddedElement;
+            internal int Sum;
+            internal int TotalWeight;
+            internal List<int> Elements;
+        }
+
+        // O(n^2*c) Time | O(n*c) Space, where n is the length of the array and c is the capacity
+        public static List<List<int> > Solution1(int[,] items, int capacity)
+        {
+            var table = new TableRegistry[capacity + 1];
+            for (int i = 0; i < table.Length; i++)
+            {
+                table[i] = new TableRegistry
+                {
+                    LastAddedElement = -1,
+                    Sum = 0,
+                    TotalWeight = 0,
+                    Elements = new List<int>()
+                };
+            }
+
+            for (int i = 0; i < items.GetLength(0); i++)
+            {
+                var currentWeight = items[i, 1];
+                for (int j = currentWeight; j < table.Length; j++)
+                {
+                    var valueToAdd = table[j - currentWeight].LastAddedElement < i ? items[i, 0] : 0; // consider new value only if not added
+                    var weightToAdd = table[j - currentWeight].LastAddedElement < i ? items[i, 1] : 0; // consider new value only if not added
+
+                    var betterValue = table[j].Sum < table[j - currentWeight].Sum + valueToAdd;
+                    var equalValue = table[j].Sum == table[j - currentWeight].Sum + valueToAdd;
+                    var betterWeight = table[j].TotalWeight > table[j - currentWeight].TotalWeight + weightToAdd;
+                    if (betterValue || equalValue && betterWeight)
+                    {
+                        table[j].LastAddedElement = i;
+                        table[j].Sum = table[j - currentWeight].Sum + valueToAdd;
+                        table[j].TotalWeight = table[j - currentWeight].TotalWeight + weightToAdd;
+                        table[j].Elements = new List<int>(table[j - currentWeight].Elements);
+                        if (valueToAdd > 0)
+                            table[j].Elements.Add(i);
+                    }
+                }
+            }
+
+            var solution = new List<List<int>>
+            {
+                new List<int>{ table[table.Length - 1].Sum},
+                table[table.Length - 1].Elements
+            };
+
+            return solution;
+        }
+
+        // O(n*c) Time | O(n*c) Space, where n is the length of the array and c is the capacity
+        public static List<List<int>> Solution2(int[,] items, int capacity)
+        {
+            if (capacity == 0 || items.Length == 0)
+            {
+                return new List<List<int>>
+                {
+                    new List<int>{0},
+                    new List<int>()
+                };
+            }
+
+            var table = new int[items.GetLength(0) + 1, capacity + 1];
+            for (int i = 1; i < table.GetLength(0); i++)
+            {
+                var value = items[i - 1, 0];
+                var weght = items[i - 1, 1];
+                for (int j = 0; j < weght; j++)
+                {
+                    table[i, j] = table[i - 1, j];
+                }
+                for (int j = weght; j < table.GetLength(1); j++)
+                {
+                    table[i, j] = Math.Max(table[i - 1, j], table[i - 1, j - weght] + value);
+                }
+            }
+
+            var x = table.GetLength(0) - 1;
+            var y = table.GetLength(1) - 1;
+            var solution = new List<int>();
+            while (table[x, y] != 0)
+            {
+                if (table[x, y] == table[x - 1, y])
+                    x--;
+                else
+                {
+                    solution.Add(x - 1);
+                    y -= items[x - 1, 1];
+                    x--;
+                }
+            }
+
+            return new List<List<int>>
+            {
+                new List<int>{table[table.GetLength(0) - 1, table.GetLength(1) - 1]},
+                solution
+            };
+        }
+    }
+}
---
+++ SandBoxTest/hard/KnapsackProblemTest.cs
@@ -0,0 +1,120 @@
+﻿namespace AlgoExpertTest.hard
+{
+    using System.Collections.Generic;
+    using AlgoExpert.hard;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class KnapsackProblemTest
+    {
+        [Test]
+        public void Solution1_TestCase1()
+        {
+            int[,] items =
+            {
+                { 1, 2 },
+                { 4, 3 },
+                { 5, 6 },
+                { 6, 7 }
+            };
+            int capacity = 10;
+
+            var expected = new List<List<int>>
+            {
+                new List<int>{10},
+                new List<int>{1, 3}
+            };
+            var actual = KnapsackProblem.Solution1(items, capacity);
+            actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+
+        [Test]
+        public void Solution1_TestCase2()
+        {
+            int[,] items =
+            {
+                {465, 100},
+                {400, 85},
+                {255, 55},
+                {350, 45},
+                {650, 130},
+                {1000, 190},
+                {455, 100},
+                {100, 25},
+                {1200, 190},
+                {320, 65},
+                {750, 100},
+                {50, 45},
+                {550, 65},
+                {100, 50},
+                {600, 70},
+                {240, 40}
+            };
+            int capacity = 200;
+
+            var expected = new List<List<int>>
+            {
+                new List<int>{1500},
+                new List<int>{3, 12, 14}
+            };
+            var actual = KnapsackProblem.Solution1(items, capacity);
+            // actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering()); - fails
+        }
+        
+        [Test]
+        public void Solution2_TestCase1()
+        {
+            int[,] items =
+            {
+                { 1, 2 },
+                { 4, 3 },
+                { 5, 6 },
+                { 6, 7 }
+            };
+            int capacity = 10;
+
+            var expected = new List<List<int>>
+            {
+                new List<int>{10},
+                new List<int>{3, 1}
+            };
+            var actual = KnapsackProblem.Solution2(items, capacity);
+            actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+
+        [Test]
+        public void Solution2_TestCase2()
+        {
+            int[,] items =
+            {
+                {465, 100},
+                {400, 85},
+                {255, 55},
+                {350, 45},
+                {650, 130},
+                {1000, 190},
+                {455, 100},
+                {100, 25},
+                {1200, 190},
+                {320, 65},
+                {750, 100},
+                {50, 45},
+                {550, 65},
+                {100, 50},
+                {600, 70},
+                {240, 40}
+            };
+            int capacity = 200;
+
+            var expected = new List<List<int>>
+            {
+                new List<int>{1500},
+                new List<int>{14, 12, 3}
+            };
+            var actual = KnapsackProblem.Solution2(items, capacity);
+            actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+    }
+}

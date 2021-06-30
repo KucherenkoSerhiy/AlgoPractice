b8d384d09b01b72e91191af15c3d49203657bcc8
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2021-06-30 14:37:40 +0200

    DiskStacking

---
+++ SandBox/hard/DiskStacking.cs
@@ -0,0 +1,55 @@
+﻿namespace AlgoExpert.hard
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class DiskStacking
+    {
+        // O(n^2) Time | O(n) Space, where n = length of the array
+        public static List<int[]> Solution(List<int[]> disks) {
+            disks.Sort((a, b) => a[2] - b[2]);
+            var diskStacks = new List<int[]>();
+            for (int i = 0; i < disks.Count; i++)
+            {
+                var diskStack = new int[3];
+                Array.Copy(disks[i], diskStack, 3);
+                diskStacks.Add(diskStack);
+            }
+
+            for (int i = 1; i < disks.Count; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (diskStacks[i][2] < diskStacks[j][2] + disks[i][2] &&
+                        disks[i][0] > disks[j][0] && disks[i][1] > disks[j][1] && disks[i][2] > disks[j][2])
+                    {
+                        diskStacks[i][0] = disks[i][0] + diskStacks[j][0];
+                        diskStacks[i][1] = disks[i][1] + diskStacks[j][1];
+                        diskStacks[i][2] = disks[i][2] + diskStacks[j][2];
+                    }
+                }
+            }
+
+            var solution = new List<int[]>();
+            int currentSolutionIndex = 0;
+            for (int i = 0; i < diskStacks.Count; i++)
+            {
+                if (diskStacks[i][2] > diskStacks[currentSolutionIndex][2])
+                    currentSolutionIndex = i;
+            }
+
+            solution.Add(disks[currentSolutionIndex]);
+            var nextDiskStackHeight = diskStacks[currentSolutionIndex][2] - disks[currentSolutionIndex][2];
+            var nextDiskStackIndex = diskStacks.FindIndex(x => x[2] == nextDiskStackHeight);
+            while (nextDiskStackIndex >= 0)
+            {
+                solution.Add(disks[nextDiskStackIndex]);
+                nextDiskStackHeight = diskStacks[nextDiskStackIndex][2] - disks[nextDiskStackIndex][2];
+                nextDiskStackIndex = diskStacks.FindIndex(x => x[2] == nextDiskStackHeight);
+            }
+            solution.Reverse();
+
+            return solution;
+        }
+    }
+}
---
+++ SandBoxTest/hard/DiskStackingTest.cs
@@ -0,0 +1,51 @@
+﻿namespace AlgoExpertTest.hard
+{
+    using System.Collections.Generic;
+    using AlgoExpert.hard;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class DiskStackingTest
+    {
+        [Test]
+        public void TestCase1()
+        {
+            var disks = new List<int[]>
+            {
+                new[] { 2, 1, 2 },
+                new[] { 3, 2, 3 },
+                new[] { 2, 2, 8 },
+                new[] { 2, 3, 4 },
+                new[] { 1, 3, 1 },
+                new[] { 4, 4, 5 },
+            };
+            var expected = new List<int[]>
+            {
+                new[] { 2, 1, 2 },
+                new[] { 3, 2, 3 },
+                new[] { 4, 4, 5 }
+            };
+            var actual = DiskStacking.Solution(disks);
+            actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+
+        [Test]
+        public void TestCase2()
+        {
+            var disks = new List<int[]>
+            {
+                new[] { 2, 1, 2 },
+                new[] { 3, 2, 3 }
+            };
+            var expected = new List<int[]>
+            {
+                new[] { 2, 1, 2 },
+                new[] { 3, 2, 3 }
+            };
+            var actual = DiskStacking.Solution(disks);
+            actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+    }
+}

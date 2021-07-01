18df1e788fb6160dfb81e9d9beda2c0468695490
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2021-07-01 16:13:26 +0200

    MaximumSumSubmatrix

---
+++ SandBox/hard/MaximumSumSubmatrix.cs
@@ -0,0 +1,42 @@
+﻿namespace AlgoExpert.hard
+{
+    using System;
+
+    public class MaximumSumSubmatrix
+    {
+        // O(n*m Time) | O(n*m) Space, where n and m are height and width of the matrix
+        public int Solution1(int[,] matrix, int size)
+        {
+            var height = matrix.GetLength(0);
+            var width = matrix.GetLength(1);
+            var sums = new int[height, width];
+            
+            // build the accumulative sum matrix
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    var topSum = i > 0 ? sums[i - 1, j] : 0;
+                    var leftSum = j > 0 ? sums[i, j - 1] : 0;
+                    var topLeftSum = i > 0 && j > 0 ? sums[i - 1, j - 1] : 0;
+                    sums[i, j] = matrix[i, j] + leftSum + topSum - topLeftSum;
+                }
+            }
+
+            int maxSum = int.MinValue;
+            for (int i = size - 1; i < height; i++)
+            {
+                for (int j = size - 1; j < width; j++)
+                {
+                    var topSum = i - size >= 0 ? sums[i - size, j] : 0;
+                    var leftSum = j - size >= 0 ? sums[i, j - size] : 0;
+                    var topLeftSum = i  - size >= 0 && j  - size >= 0 ? sums[i - size, j - size] : 0;
+                    var sum = sums[i, j] + topLeftSum - topSum - leftSum;
+                    maxSum = Math.Max(maxSum, sum);
+                }
+            }
+            
+            return maxSum;
+        }
+    }
+}
---
+++ SandBoxTest/hard/MaximumSumSubmatrixTest.cs
@@ -0,0 +1,27 @@
+﻿namespace AlgoExpertTest.hard
+{
+    using AlgoExpert.hard;
+    using NUnit.Framework;
+
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class MaximumSumSubmatrixTest
+    {
+        [Test]
+        public void TestCase1()
+        {
+            int[,] matrix = 
+            {
+                { 5, 3, -1, 5},
+                { -7, 3, 7, 4},
+                { 12, 8, 0, 0},
+                { 1, -8, -8, 2}
+            };
+            int size = 2;
+            int expected = 18;
+            var sut = new MaximumSumSubmatrix();
+            int actual = sut.Solution1(matrix, size);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

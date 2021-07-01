7a89e71131a2aba63d10e75117318160ac1a63bc
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2021-07-02 00:47:53 +0200

    MaxPathSum

---
+++ SandBox/hard/MaxPathSum.cs
@@ -0,0 +1,40 @@
+﻿namespace AlgoExpert.hard
+{
+    using System;
+
+    public class MaxPathSum
+    {
+        // O(n) Time | O(log(n)) Space, where n = number of nodes
+        public static int Solution(BinaryTree tree)
+        {
+            var subtreeMaxSum = int.MinValue;
+            var currentNodeSum = SubtreeMaxPathSum(tree, ref subtreeMaxSum);
+            return Math.Max(currentNodeSum, subtreeMaxSum);
+        }
+
+        private static int SubtreeMaxPathSum(BinaryTree tree, ref int subtreeMaxSum)
+        {
+            if (tree == null) return 0;
+            var leftSum = SubtreeMaxPathSum(tree.left, ref subtreeMaxSum);
+            var rightSum = SubtreeMaxPathSum(tree.right, ref subtreeMaxSum);
+
+            var currentNodeSum = tree.value + (leftSum > 0 ? leftSum : 0) 
+                                            + (rightSum > 0 ? rightSum : 0);
+            subtreeMaxSum = Math.Max(currentNodeSum, subtreeMaxSum);
+
+            var subtreeSum = Math.Max(leftSum, rightSum);
+            if (subtreeSum < 0) subtreeSum = 0;
+            return tree.value + subtreeSum;
+        }
+
+        public class BinaryTree {
+            public int value;
+            public BinaryTree left;
+            public BinaryTree right;
+
+            public BinaryTree(int value) {
+                this.value = value;
+            }
+        }
+    }
+}
---
+++ SandBoxTest/hard/MaxPathSumTest.cs
@@ -0,0 +1,141 @@
+﻿namespace AlgoExpertTest.hard
+{
+    using AlgoExpert.hard;
+    using NUnit.Framework;
+
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class MaxPathSumTest
+    {
+        [Test]
+        public void TestCase1()
+        {
+            var tree = new MaxPathSum.BinaryTree(1)
+            {
+                left = new MaxPathSum.BinaryTree(2)
+                {
+                    left = new MaxPathSum.BinaryTree(4),
+                    right = new MaxPathSum.BinaryTree(5)
+                },
+                right = new MaxPathSum.BinaryTree(3)
+                {
+                    left = new MaxPathSum.BinaryTree(6),
+                    right = new MaxPathSum.BinaryTree(7)
+                }
+            };
+            var expected = 18;
+            var actual = MaxPathSum.Solution(tree);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestCase2()
+        {
+            var tree = new MaxPathSum.BinaryTree(1)
+            {
+                left = new MaxPathSum.BinaryTree(2),
+                right = new MaxPathSum.BinaryTree(-1)
+            };
+            var expected = 3;
+            var actual = MaxPathSum.Solution(tree);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestCase3()
+        {
+            var tree = new MaxPathSum.BinaryTree(-2)
+            {
+                left = new MaxPathSum.BinaryTree(-1)
+                {
+                    left = new MaxPathSum.BinaryTree(2),
+                    right = new MaxPathSum.BinaryTree(3)
+                }
+            };
+            var expected = 4;
+            var actual = MaxPathSum.Solution(tree);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestCase4()
+        {
+            var tree = new MaxPathSum.BinaryTree(1)
+            {
+                left = new MaxPathSum.BinaryTree(-5)
+                {
+                    left = new MaxPathSum.BinaryTree(0)
+                    {
+                        left = new MaxPathSum.BinaryTree(-3)
+                        {
+                            left = new MaxPathSum.BinaryTree(0)
+                            {
+                                left = new MaxPathSum.BinaryTree(3),
+                                right = new MaxPathSum.BinaryTree(1)
+                            },
+                            right = new MaxPathSum.BinaryTree(1)
+                            {
+                                left = new MaxPathSum.BinaryTree(2),
+                                right = new MaxPathSum.BinaryTree(2)
+                            }
+                        },
+                        right = new MaxPathSum.BinaryTree(-4)
+                        {
+                            left = new MaxPathSum.BinaryTree(3)
+                            {
+                                left = new MaxPathSum.BinaryTree(7),
+                                right = new MaxPathSum.BinaryTree(-5)
+                            },
+                            right = new MaxPathSum.BinaryTree(-1)
+                        }
+                    },
+                    right = new MaxPathSum.BinaryTree(2)
+                    {
+                        left = new MaxPathSum.BinaryTree(1)
+                        {
+                            left = new MaxPathSum.BinaryTree(-1),
+                            right = new MaxPathSum.BinaryTree(-6)
+                        },
+                        right = new MaxPathSum.BinaryTree(1)
+                        {
+                            left = new MaxPathSum.BinaryTree(-1),
+                            right = new MaxPathSum.BinaryTree(-100)
+                        }
+                    }
+                },
+                right = new MaxPathSum.BinaryTree(3)
+                {
+                    left = new MaxPathSum.BinaryTree(2)
+                    {
+                        left = new MaxPathSum.BinaryTree(0)
+                        {
+                            left = new MaxPathSum.BinaryTree(-9),
+                            right = new MaxPathSum.BinaryTree(-91)
+                        },
+                        right = new MaxPathSum.BinaryTree(5)
+                        {
+                            left = new MaxPathSum.BinaryTree(2),
+                            right = new MaxPathSum.BinaryTree(1)
+                        }
+                    },
+                    right = new MaxPathSum.BinaryTree(1)
+                    {
+                        left = new MaxPathSum.BinaryTree(1)
+                        {
+                            left = new MaxPathSum.BinaryTree(0),
+                            right = new MaxPathSum.BinaryTree(1)
+                        },
+                        right = new MaxPathSum.BinaryTree(1)
+                        {
+                            left = new MaxPathSum.BinaryTree(-5),
+                            right = new MaxPathSum.BinaryTree(0)
+                        }
+                    }
+                }
+            };
+            var expected = 15;
+            var actual = MaxPathSum.Solution(tree);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

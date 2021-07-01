dec0bcc87d6d392c3ab95ea81504f09d71a0d402
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2021-07-01 16:13:15 +0200

    NumbersInPi

---
+++ SandBox/hard/NumbersInPi.cs
@@ -0,0 +1,39 @@
+﻿namespace AlgoExpert.hard
+{
+    using System.Collections.Generic;
+
+    public static class NumbersInPi
+    {
+        public static int Solution(string pi, string[] numbers)
+        {
+            var memo = new Dictionary<string, int>();
+            var substrings = Imm_Substrings(pi, numbers, ref memo);
+            if (substrings > 0) substrings--; // spaces = substrings -1
+            return substrings;
+        }
+
+        // O(n^3 + m) Time | O(n*m) Space, where n = length of the pi array and m = length of the numbers array
+        private static int Imm_Substrings(string pi, string[] numbers, ref Dictionary<string, int> memo)
+        {
+            if (string.IsNullOrEmpty(pi)) 
+                return 0;
+            if (memo.ContainsKey(pi))
+                return memo[pi];
+
+            var minSpaces = -1;
+            foreach (var number in numbers)
+            {
+                if (pi.StartsWith(number))
+                {
+                    var suffixSpaces = Imm_Substrings(pi.Substring(number.Length), numbers, ref memo);
+                    if (suffixSpaces != -1 && (minSpaces == -1 || suffixSpaces < minSpaces))
+                        minSpaces = suffixSpaces + 1;
+                }
+            }
+
+            if (minSpaces != -1 && !memo.ContainsKey(pi))
+                memo.Add(pi, minSpaces);
+            return minSpaces;
+        }
+    }
+}
---
+++ SandBoxTest/hard/NumbersInPiTest.cs
@@ -0,0 +1,40 @@
+﻿namespace AlgoExpertTest.hard
+{
+    using AlgoExpert.hard;
+    using NUnit.Framework;
+
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class NumbersInPiTest
+    {
+        [Test]
+        public void TestCase1()
+        {
+            var pi = "3141592";
+            string[] numbers = {"3141", "5", "31", "2", "4159", "9", "42"};
+            var expected = 2;
+            var actual = NumbersInPi.Solution(pi, numbers);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestCase2()
+        {
+            var pi = "3141592653589793238462643383279";
+            string[] numbers = {"3141", "1512", "159", "793", "12412451", "8462643383279"};
+            var expected = -1;
+            var actual = NumbersInPi.Solution(pi, numbers);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestCase3()
+        {
+            var pi = "3141592653589793238462643383279";
+            string[] numbers = {"3", "1", "4", "592", "65", "55", "35", "8", "9793", "2384626", "83279"};
+            var expected = 13;
+            var actual = NumbersInPi.Solution(pi, numbers);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

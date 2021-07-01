72d830117c54487838367cc88d4b9f0ca84b22e8
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2021-07-01 17:02:13 +0200

    ValidateThreeNodes

---
+++ SandBox/hard/ValidateThreeNodes.cs
@@ -0,0 +1,42 @@
+﻿namespace AlgoExpert.hard
+{
+    public class ValidateThreeNodes
+    {
+        public class BST {
+            public int value;
+            public BST left = null;
+            public BST right = null;
+
+            public BST(int value) {
+                this.value = value;
+            }
+        }
+
+        // O(d) Time | O(1) Space, where d = depth of the tree
+        public bool Solution(BST nodeOne, BST nodeTwo, BST nodeThree)
+        {
+            bool isValid = this.Validate(nodeOne, nodeTwo, nodeThree);
+            if (!isValid)
+                isValid = this.Validate(nodeThree, nodeTwo, nodeOne);
+            return isValid;
+        }
+
+        private bool Validate(BST nodeOne, BST nodeTwo, BST nodeThree)
+        {
+            var currentNode = nodeOne;
+            this.SearchNode(ref currentNode, nodeTwo);
+
+            if (currentNode != null)
+                this.SearchNode(ref currentNode, nodeThree);
+            return currentNode == nodeThree;
+        }
+
+        private void SearchNode(ref BST currentNode, BST targetNode)
+        {
+            while (currentNode != null && currentNode != targetNode)
+            {
+                currentNode = targetNode.value < currentNode.value ? currentNode.left : currentNode.right;
+            }
+        }
+    }
+}
---
+++ SandBoxTest/hard/ValidateThreeNodesTest.cs
@@ -0,0 +1,40 @@
+﻿namespace AlgoExpertTest.hard
+{
+    using AlgoExpert.hard;
+    using NUnit.Framework;
+
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class ValidateThreeNodesTest
+    {
+        [Test]
+        public void TestCase1()
+        {
+            var nodeOne = new ValidateThreeNodes.BST(5)
+            {
+                right = new ValidateThreeNodes.BST(7)
+                {
+                    left = new ValidateThreeNodes.BST(6),
+                    right = new ValidateThreeNodes.BST(8)
+                }
+            };
+            var nodeThree = new ValidateThreeNodes.BST(3);
+            var nodeTwo = new ValidateThreeNodes.BST(2)
+            {
+                left = new ValidateThreeNodes.BST(1)
+                {
+                    left = new ValidateThreeNodes.BST(0)
+                },
+                right = new ValidateThreeNodes.BST(4)
+                {
+                    left = nodeThree
+                }
+            };
+            var bst = nodeOne;
+            bst.left = nodeTwo;
+
+            var sut = new ValidateThreeNodes();
+            Assert.IsTrue(sut.Solution(nodeOne, nodeTwo, nodeThree));
+        }
+    }
+}

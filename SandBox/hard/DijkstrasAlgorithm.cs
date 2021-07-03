73f33982eaca4c5abdcbfc6009ef8867c3ec7381
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2021-07-03 23:31:34 +0200

    DijkstrasAlgorithm

---
+++ SandBox/hard/DijkstrasAlgorithm.cs
@@ -0,0 +1,48 @@
+﻿namespace AlgoExpert.hard
+{
+    public class DijkstrasAlgorithm
+    {
+        // O(V*E + V^2) Time | O(V) Space, where V = vertices and E = edges of the graph
+        public int[] Solution(int start, int[][][] edges)
+        {
+            // init data structure
+            int[] distances = new int[edges.Length];
+            for (int i = 0; i < distances.Length; i++)
+                distances[i] = -1;
+            distances[start] = 0;
+
+            bool[] visited = new bool[edges.Length];
+
+            int currentVertice = start;
+
+            while (currentVertice != -1)
+            {
+                // count edges
+                foreach (var currentVerticeEdges in edges[currentVertice])
+                {
+                    var targetVertice = currentVerticeEdges[0];
+                    var currentDistance = distances[currentVertice] + currentVerticeEdges[1];
+                    if (distances[targetVertice] == -1 || distances[targetVertice] > currentDistance)
+                        distances[targetVertice] = currentDistance;
+                }
+
+                visited[currentVertice] = true;
+                
+                // TODO: improvable: use min heap to change this O(V) to O(log(V))
+                // select next vertice
+                currentVertice = -1;
+                var minEdgeDistance = int.MaxValue;
+                for (int i = 0; i < distances.Length; i++)
+                {
+                    if (!visited[i] && distances[i] != -1 && distances[i] < minEdgeDistance)
+                    {
+                        currentVertice = i;
+                        minEdgeDistance = distances[i];
+                    }
+                }
+            }
+
+            return distances;
+        }
+    }
+}
---
+++ SandBoxTest/hard/DijkstrasAlgorithmTest.cs
@@ -0,0 +1,44 @@
+﻿namespace AlgoExpertTest.hard
+{
+    using AlgoExpert.hard;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class DijkstrasAlgorithmTest
+    {
+        [Test]
+        public void TestCase1()
+        {
+            int start = 0;
+            int[][][] edges =
+            {
+                new[]
+                {
+                    new [] {1, 7}
+                },
+                new []
+                {
+                    new [] {2, 6},
+                    new [] {3, 20},
+                    new [] {4, 3}
+                },
+                new[]
+                {
+                    new [] {3, 14}
+                },
+                new[]
+                {
+                    new [] {4, 2}
+                },
+                new int[0][],
+                new int[0][]
+            };
+            int[] expected = {0, 7, 13, 27, 10, -1};
+            var sut = new DijkstrasAlgorithm();
+            var actual = sut.Solution(start, edges);
+            actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+    }
+}
